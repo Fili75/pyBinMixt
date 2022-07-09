6a40c5ffec9786c1b683bf701c4edc3e2856be68
Fili75 <skelegh78@gmail.com>
2022-07-09 10:59:30 +0200

    setup.py modified

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="pyBinMixt",
     version="1.0.0",  # version number is set in pyMixtComp/_version.py
     author="Filippo Antonazzo",
-    description="Frugal bin-marginal Gaussian-based clustering",
+    description="Frugal bin-marginal Gaussian model-based clustering",
     keywords=["clustering", "mixture model", "heterogeneous", "missing data"],
     classifiers=[
         "Programming Language :: C++",
